2ddffb77f4b5d8f568e6115c1304692347eeb851
huayangchen <503952824@qq.com>
2018-08-23 22:09:36 +0800

    新增props默认复制和约束限制

--- pages/homes/adCodePage.js
+++ pages/homes/adCodePage.js
@@ -1,15 +1,22 @@
 import React, {Component} from 'react'
-import {View,Text,Image} from 'react-native'
+import {View,Text,Image,} from 'react-native'
 import QRCode from 'react-native-qrcode';
-
+import PropTypes from 'prop-types';
 const Dimensions = require('Dimensions');
 
+
 export default class adCodePage extends Component {
+    //给props赋予默认值
+    static defaultProps={
+        qrcode:"https://my.oschina.net/huayangchen"
+    }
+    //约束props类型
+    static propTypes={
+        qrcode:PropTypes.string.isRequired
+    }
+
     constructor(){
         super();
-        this.defaultProps={
-            qrcode:'https://my.oschina.net/huayangchen'
-        }
     }
 
     render() {
@@ -18,7 +25,7 @@ export default class adCodePage extends Component {
                 <Text style={{textAlign: 'center',marginTop:70}}>{this.props.qrcode}</Text>
                 <View style={{flex:1,marginLeft:Dimensions.get('window').width/4,marginTop:10}}>
                 <QRCode
-                    value={this.state.qrcode}
+                    value={this.props.qrcode}
                     size={Dimensions.get('window').width/2}
                     bgColor={'black'}
                     fgColor={'white'}
